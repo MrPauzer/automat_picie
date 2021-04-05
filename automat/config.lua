8900fad6ebdaa64bd8d681da1c51ae1a8e3d7d60
Pauzer <63166329+MrPauzer@users.noreply.github.com>
2021-04-05 16:40:35 +0200

    Add files via upload

---
+++ automat/config.lua
@@ -0,0 +1,9 @@
+-----------------------
+-----MrPauzer#5567-----
+-----------------------
+
+Config = {}
+
+Config.WaterPrice = 2 -- Cena 
+Config.TankModel = -742198632 -- Model automatu
+Config.DrinkingTime = 10000 -- czas picia
